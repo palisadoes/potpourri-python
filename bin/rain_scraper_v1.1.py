3a72512f2dba435a23d4fcd58fe8cea334b9479e
Peter Harrison <peter@colovore.com>
2021-10-28 17:55:01 -0700

    Initial commit

--- bin/rain_scraper_v1.1.py
+++ bin/rain_scraper_v1.1.py
@@ -282,7 +282,7 @@ def get_data(url):
     log.log2debug(2003, log_message)
 
     # Contientious sleep
-    time.sleep(1 + (random.random() * 2))
+    time.sleep(1 + (random.random() * 1))
 
     # Read data
     try:
